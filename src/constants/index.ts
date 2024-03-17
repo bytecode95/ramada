3a007271cfefaf5edaba93f6f4947da80e657502
chethana95 <chethana.virajini@upview.tech>
2024-03-17 19:16:07 +0530

    Constant Details Update

---
+++ src/constants/index.ts
@@ -0,0 +1,98 @@
+// Navbar section
+export const NAV_LINKS = [
+  { href: "/#accommodation", key: "accommodation", label: "Accommodation" },
+  { href: "/#restaurants", key: "restaurants", label: "Restaurants" },
+  { href: "/#banquets", key: "banquets", label: "Banquets" },
+  { href: "/#amenities", key: "amenities", label: "Amenities" },
+  { href: "/#explore", key: "explore", label: "Explore Us" },
+  { href: "/#delivery", key: "delivery", label: "Delivery" },
+];
+
+export const HERO = [
+  {
+    title: "Lorem Ipsum is simply dummy text of the printing ",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown.",
+  },
+];
+
+export const ABOUT = [
+  {
+    title: "explore more about us",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown .Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown .Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown .Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+  },
+];
+
+export const ACCOMMODATION = [
+  {
+    image: "/images/For-Accommodation-F1.png",
+    title: "twin bedded deluxe room",
+    facilities: [
+      "Lorem Ipsum",
+      "Typesetting industry.",
+      "Lorem Ipsum has been",
+      "The industry's standard",
+    ],
+  },
+  {
+    image: "/images/For-Accommodation-F2.png",
+    title: "King Bed Deluxe Room",
+    facilities: [
+      "Lorem Ipsum",
+      "Typesetting industry.",
+      "Lorem Ipsum has been",
+      "The industry's standard",
+    ],
+  },
+  {
+    image: "/images/For-Accommodation-F3.png",
+    title: "Splendor Suite",
+    facilities: [
+      "Lorem Ipsum",
+      "Typesetting industry.",
+      "Lorem Ipsum has been",
+      "The industry's standard",
+    ],
+  },
+  {
+    image: "/images/For-Accommodation-F4.png",
+    title: "Premier Suite",
+    facilities: [
+      "Lorem Ipsum",
+      "Typesetting industry.",
+      "Lorem Ipsum has been",
+      "The industry's standard",
+    ],
+  },
+];
+
+export const FacilityImages = [
+  { src: "/images/For-Facility-F1.png", alt: "img1" },
+  { src: "/images/For-Facility-F2.png", alt: "img2" },
+  { arc: "/images/For-Facility-F3.png", alt: "img3" },
+  { src: "/images/For-Facility-F4.png", alt: "img4" },
+  { src: "/images/For-Facility-F5.png", alt: "img5" },
+];
+
+export const facilityTexts = [
+  "Gym",
+  "Ayu Balinese Spa",
+  "Sauna",
+  "Steam",
+  "Swimming Pool",
+];
+
+export const DINERS = [
+  { src: "/images/For-Dining-F1.png", alt: "img1", text:'The Gardenia Coffee Shop' },
+  { src: "/images/For-Dining-F2.png", alt: "img2", text:'Alhambra' },
+  { src: "/images/For-Dining-F3.png", alt: "img3", text:'Oasis Lounge' },
+  { src: "/images/For-Dining-F4.png", alt: "img4", text:'The Secret Garden ' },
+  { src: "/images/For-Dining-F5.png", alt: "img5", text:'Baked' },
+];
+
+export const BANQUETS = [
+  { src: "/images/For-Banquets-F1.png", alt: "img1", title:'The Liberty Ballroom',  text:'An idyllic setting for any grand wedding celebration or special occasion. Liberty Ball Room offers a spacious and lavish ambience to ensure a truly memorable occasion.' },
+  { src: "/images/For-Banquets-F2.png", alt: "img2", title:'The Liberty Ballroom',  text:'A spacious meeting room with an aquarium style view of the streets of Colombo.' },
+  { src: "/images/For-Banquets-F3.png", alt: "img3", title:'The Liberty Ballroom',  text:'A private meeting space equipped with state of the art technology.' },
+];
