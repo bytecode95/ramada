1ab5add2bb0e745ba6944ddf2d1870200a6ab657
chethana95 <chethana.virajini@upview.tech>
2024-03-18 09:27:46 +0530

    Section updated

--- src/components/Deliver/Deliver.tsx
+++ src/components/Deliver/Deliver.tsx
@@ -1,8 +1,38 @@
+import Button from "../shared/Button"
 
 
 function Deliver() {
   return (
-    <div>Deliver</div>
+    <section className="max-width-container ">
+      <div className="main-width-container padding-container">
+        <div className="flex justify-between items-center flex-wrap-reverse">
+          <div className="w-full lg:w-1/2">
+            <h4 className="section-header-text">Delivery & Takeaway service</h4>
+            <h1 className="section-topic-text">Giving your hunger a safer, faster and better alternative</h1>
+            <p className="section-desc-text text-start mt-4">
+              users can pay directly through our website with Visa or MasterCard or opt for cash or card payment on delivery! Our website is extremely user friendly and offers a stress-free ordering experience with the option to schedule a delivery on a particular date or time. Our delivery radius will be from Colombo 1-15 and will include areas in Mount Lavinia, Nawala, Nugegoda, Kohuwala, Dehiwala, Rajagiriya, Welikada, Wellampitiya, Ethul Kotte, Kolonnawa, Sri Jayawardenapura Kotte, Pelawatta, Kotikawatta, Battaramulla, Pita Kotte, Attidiya, Mirihana, Koswatta.If you reside in any of the above locations, orders will be delivered subjected to a delivery charge.
+            </p>
+            <p>Delivery times are between 10 am to 10 pm. </p>
+            <p>For more information contact us on (+94) 112 422 001, Ext. 399 or (+94) 777 864 864. </p>
+            <Button
+              type="button"
+              title="Order Now"
+              variant="btn_Buy_Now"
+            />
+          </div>
+          <div className="mx-auto mt-5 md:mx-0">
+            <img
+              src='/images/Delivery.png'
+              width={500}
+              height={500}
+              alt="helping-img"
+              className="mobile-img-container"
+            />
+          </div>
+
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/Explore/Explore.tsx
+++ src/components/Explore/Explore.tsx
@@ -1,8 +1,32 @@
+import { BsArrowRight } from "react-icons/bs"
+import Button from "../shared/Button"
 
 
 function Explore() {
   return (
-    <div>Explore</div>
+    <section className="max-width-container ">
+      <div className="main-width-container padding-container">
+        <h4 className="section-header-text">High Tea</h4>
+        <div className="flex justify-between items-center flex-wrap-reverse">
+          <div className="mx-auto mt-5 md:mx-0">
+            <img
+              src='/images/ExploreHighTea.png'
+              width={688}
+              height={500}
+              alt="helping-img"
+              className="mobile-img-container"
+            />
+          </div>
+          <div className="w-full lg:w-1/2">
+            <h1 className="section-topic-text">The Ramada Colombo's high tea buffe</h1>
+            <p className="section-desc-text text-start mt-4">
+            The ambience at the Ramada lobby is no less than what you’d expect from a real classy high tea. You can listen to classical instrumentals of your favorite songs from live violin and piano music. The guests were in awe at how this instrumental duo filled the whole atmosphere with their tunes, while they indulged in buffet delicacies.
+            </p>
+            <Button type="button" title="Explore Ramada High tea" variant="btn_Learn_More" icon={<BsArrowRight />} />
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
