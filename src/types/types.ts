333da170d59e7cca5782d2b0186ca6a9e77581f1
chethana95 <chethana.virajini@upview.tech>
2024-03-18 11:27:45 +0530

    types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,15 +1,31 @@
 import { ReactNode } from "react";
 
 export type ButtonProps = {
-    type: 'button' | 'submit';
-    title: string;
-    variant: string;
-    handleClick?: () => void;
-    icon?: ReactNode;
+  type: "button" | "submit";
+  title: string;
+  variant: string;
+  handleClick?: () => void;
+  icon?: ReactNode;
 };
 
 export type LogoProps = {
-    height?: number;
-    width?: number;
-    handleClick?: () => void;
-}
+  height?: number;
+  width?: number;
+  handleClick?: () => void;
+};
+
+export type RatingProps = {
+  maxRating: number;
+  rating: number;
+  readOnly?: boolean;
+  size?: string;
+};
+
+export type SubscriptionFormProps = {
+  onSubmit: (email: string) => void;
+};
+
+export type SocialButtonProps = {
+  name: string;
+  url: string;
+};
