5a4989b7830fadc195342a585f314a64d0ea64f2
chethana95 <chethana.virajini@upview.tech>
2024-03-18 09:26:27 +0530

    Type alias

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,8 +1,11 @@
+import { ReactNode } from "react";
+
 export type ButtonProps = {
     type: 'button' | 'submit';
     title: string;
     variant: string;
     handleClick?: () => void;
+    icon?: ReactNode;
 };
 
 export type LogoProps = {
