733e17216b721f6a7f35c2e287efbb5fcbadfe9a
chethana95 <chethana.virajini@upview.tech>
2024-03-18 09:25:58 +0530

    Accommodation section

--- src/components/Accommodation/Accommodation.tsx
+++ src/components/Accommodation/Accommodation.tsx
@@ -1,8 +1,44 @@
+import { ACCOMMODATION } from "../../constants"
+import Button from "../shared/Button"
 
 
 function Accommodation() {
   return (
-    <div>Accommodation</div>
+    <section className="max-width-container">
+      <div className="main-width-container padding-container">
+        <div className=''>
+          <header className="text-start">
+            <h3 className="section-header-text">Accommodation</h3>
+            <h1 className="section-topic-text">Comfortable Rooms Just For You </h1>
+          </header>
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-3 lg:gap-4 mt-8 ">
+            {ACCOMMODATION.map((value, index) => (
+              <article key={index} className="justify-self-start">
+                <div className="w-full p-4 bg-white custom-shadow-card rounded-md">
+                  <div className="rounded-2xl shadow-lg">
+                    <img
+                      src={value.image}
+                      alt='images'
+                      width={305}
+                      height={210}
+                      style={{ width: 'auto', height: 'auto' }}
+                    />
+                  </div>
+                  <h1 className="font-[cormorant] font-bold text-center text-[22px] leading-6 py-4">{value.title}</h1>
+                  <ul className="list-disc list-inside font-[300] text-base mb-4">
+                    {value.facilities.map((facility, index) => (
+                      <li key={index}>{facility}</li>
+                    ))}
+                  </ul>
+                  <Button type="button" title="Explore" variant="btn_Explore" />
+                  <Button type="button" title="Book Now" variant="btn_Book_Now"/>
+                </div>
+              </article>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
