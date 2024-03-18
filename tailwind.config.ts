384d624be664c8290b8ba8f3f4eacb1984cd4844
chethana95 <chethana.virajini@upview.tech>
2024-03-18 09:24:16 +0530

    Custom configuration added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "hero-image": "url('/images/HeroImg.png')",
+      },
       colors: {
-        pink: { 500: '#D11242' },
-        gray: {300:'#848484', 400: '#555555', 600: '#19191A'},
+        pink: { 500: "#D11242" },
+        gray: {
+          300: "#848484",
+          400: "#555555",
+          600: "#19191A",
+          700: "#585858",
+        },
       },
       fontFamily: {
-        OpenSans: ["Open Sans"],
-        CormorantGaramond: ['Cormorant Garamond'],    
-    },
+        cormorant: ["Cormorant Garamond"],
+      },
+      screens: {
+        "4xl": "1728px",
+      },
     },
   },
   plugins: [],
-}
+};
