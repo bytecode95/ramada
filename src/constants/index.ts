59a202e950a06f88b222f8035db1ed942ab624cd
chethana95 <chethana.virajini@upview.tech>
2024-03-18 09:26:16 +0530

    Project values

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -96,3 +96,49 @@ export const BANQUETS = [
   { src: "/images/For-Banquets-F2.png", alt: "img2", title:'The Liberty Ballroom',  text:'A spacious meeting room with an aquarium style view of the streets of Colombo.' },
   { src: "/images/For-Banquets-F3.png", alt: "img3", title:'The Liberty Ballroom',  text:'A private meeting space equipped with state of the art technology.' },
 ];
+
+export const FEEDBACKS = [
+  { src:"/images/user1.png" , alt:'avatar', name:'- John Doe', comment:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown .Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.", rating: 5},
+  { src:"/images/user2.png" , alt:'avatar', name:'- Jane Doe', comment:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown .Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.", rating: 3},
+  { src:"/images/user3.png" , alt:'avatar', name:'- John Doe', comment:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown .Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.", rating: 5},
+  { src:"/images/user4.png" , alt:'avatar', name:'- John Doe', comment:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown .Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.", rating: 4}
+];
+
+//Footer section
+export const FOOTER_LINKS = [
+  {
+      title: 'Links',
+      links: [
+          { name: 'Home', type: 'internal', url: '/#restaurant' },
+          { name: 'Spa', type: 'internal', url: '/#diners' },
+          { name: 'Overview', type: 'internal', url: '/#diners' },
+          { name: 'Banquets', type: 'internal', url: '/#diners' },
+          { name: 'Accommodation', type: 'internal', url: '/#diners' },
+          { name: 'Facilities', type: 'internal', url: '/#diners' },
+          { name: 'Attractions', type: 'internal', url: '/#diners' },
+          { name: 'Wine & Dine', type: 'internal', url: '/#diners' },
+          { name: 'Promotions', type: 'internal', url: '/#diners' },
+          { name: 'Booking', type: 'internal', url: '/#diners' },
+          { name: 'Blog', type: 'internal', url: '/#diners' },
+          { name: 'Gallery', type: 'internal', url: '/#diners' },
+          { name: 'Site map', type: 'internal', url: '/#diners' },
+      ],
+  },
+  {
+      title: 'Social',
+      links: [
+          { name: 'Facebook', type: 'external', url: 'https://www.facebook.com/' },
+          { name: 'Instagram', type: 'external', url: 'https://www.instagram.com/' },
+          { name: 'X (Twitter)', type: 'external', url: 'https://twitter.com/' },
+      ],
+  },
+  {
+      title: 'RAMADA BY WYNDHAM COLOMBO',
+      links: [
+          { name: 'info@ramadacolombo.com', type: 'email', url: 'mailto:info@ramadacolombo.com' },
+          { name: '+94 11-242 2001', type: 'tel', url: 'tel:+9411-2422001' },
+          { name: '+94 76 6645507', type: 'tel', url: 'tel: +94766645507' },
+          { name: '+94 11-244 7977', type: 'tel', url: 'tel:+9411-2447977' },
+      ],
+  },
+];
