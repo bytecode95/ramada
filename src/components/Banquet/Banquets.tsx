e2c06cb0e7f060d29a865efb4aef58d405251301
chethana95 <chethana.virajini@upview.tech>
2024-03-18 19:47:37 +0530

    Responsiveness Updated

--- src/components/Banquet/Banquets.tsx
+++ src/components/Banquet/Banquets.tsx
@@ -13,7 +13,7 @@ function Banquets() {
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 2xl:gap-6 mt-8">
           {BANQUETS.map((diner, index) => (
             <div key={index} className="shadow-md flex flex-col justify-between">
-              <img src={diner.src} alt={diner.alt} width={448} height={305}  style={{width:'100%', height:'100%'}}/>
+              <img src={diner.src} alt={diner.alt} width={448} height={305} className="mx-auto"/>
               <div className="flex flex-col justify-between pb-6">
                 <div>
                   <h1 className="text-center text-[36px] font-bold py-4">{diner.title}</h1>
